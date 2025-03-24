924740ed761d6196d840ab64a73170e57a8da6d0
Toni <fazendeirotoni@gmail.com>
2025-03-24 12:09:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import 'dotenv/config'
+import connectDB from './database/client.js'
+import authRouter from './routes/auth.js'
+
+const app = express()
+
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use('/api', authRouter)
+
+
+app.get('/', (req, res) => {
+    res.send("bienvenue ici")
+})
+
+connectDB()
+app.listen(PORT, () => {
+    console.log(`Le server tourne sur le port ${PORT}`)
+})
+
